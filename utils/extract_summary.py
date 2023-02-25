94532001e93a43c1788f0bb487646a36afc88147
Arya Chakraborty <aryachakraborty2002@gmail.com>
2023-02-25 12:43:13 +0530

    extract_summary added

--- utils/extract_summary.py
+++ utils/extract_summary.py
@@ -1,10 +1,9 @@
 import os
 import cohere
 
-# COHERE_API_KEY = f"{os.getenv('cohere_api_key')}"
-COHERE_API_KEY = 'zO6ESR3o95dtHaNe2g61JLLFLr6EnGw1B9UeEmqd'
-
+COHERE_API_KEY = f"{os.getenv('cohere_api_key')}"
 co_client = cohere.Client(COHERE_API_KEY)
+
 def make_summary(text) :
     response = co_client.summarize(
         text=text,
